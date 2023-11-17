d04a6379291310aef22f9c4dc7380bf1da7fd99d
SkohTV <skookie.omg.lol@gmail.com>
2023-11-17 08:43:33 +0100

    treewide: up

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -177,6 +177,36 @@
     127.0.0.1 php.localhost
   '';
 
+  ## Nvidia
+  # Enable OpenGL
+  #hardware.opengl = {
+  #  enable = true;
+  #  driSupport = true;
+  #  driSupport32Bit = true;
+  #};
+
+  # Load nvidia driver for Xorg and Wayland
+  #services.xserver.videoDrivers = ["nvidia"];
+
+  #hardware.nvidia = {
+  #  modesetting.enable = true;
+  #  powerManagement.enable = false;
+  #  powerManagement.finegrained = false;
+  #  open = false;
+  #  nvidiaSettings = true;
+  #  package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  #};
+
+	#hardware.nvidia.prime = {
+  #  sync.enable = true;
+
+	#	intelBusId = "PCI:1:0:0";
+	#	nvidiaBusId = "PCI:0:2:0";
+	#};
+
+
+  #hardware.nvidia.forceFullCompositionPipeline = true;
+
 
   ### END
 
