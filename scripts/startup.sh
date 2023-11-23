3b01313ac4eb332e593d1babe5989a9d3e38e503
SkohTV <skookie.omg.lol@gmail.com>
2023-11-23 20:05:01 +0100

    treewide: wayland begone !

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 
+i3-msg 'workspace 5'
+alacritty -e zsh -c "sh $HOME/dev/scripts/zellij-init.sh nvim; exec $SHELL" &
+sleep 0.3
 
-
-i3-msg 'workspace 5; exec alacritty'
-sleep 0.5
-i3-msg 'workspace 6; exec alacritty'
-sleep 0.5
+i3-msg 'workspace 6'
+alacritty -e zsh -c "sh $HOME/dev/scripts/zellij-init.sh main; exec $SHELL" &
+sleep 0.3
 
 i3-msg 'exec github-desktop'
 i3-msg 'exec obsidian'
