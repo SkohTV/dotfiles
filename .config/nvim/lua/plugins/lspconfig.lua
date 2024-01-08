bd74e81cb269cbc1b90a31ca68f85a24801e995f
SkohTV <skookie.omg.lol@gmail.com>
2024-01-08 02:23:44 +0100

    treewide: patched lsp(s)

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -23,28 +23,68 @@ local config = function()
 		vim.keymap.set("n", "<leader>nd", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
 		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
     vim.keymap.set("n", "<leader>so", "<cmd>Lspsaga outline<CR>", opts) -- toggle symbols outline
+    vim.keymap.set("n", "<leader>fmt", "<cmd>lua vim.lsp.buf.format()<CR>", opts) -- force formatting of file
 	end
 
+  -- Python
+  lspconfig.pyright.setup({
+    capabilities = capabilities, on_attach = on_attach,
+    init_options = { settings = { args = { "" } } }
+  })
+  local ruff_l = require('efmls-configs.linters.ruff')
+  local ruff_f = require('efmls-configs.formatters.ruff')
+
+  -- Rust
+  lspconfig.rust_analyzer.setup({
+    capabilities = capabilities, on_attach = on_attach,
+    settings = { ['rust-analyzer'] = {} }
+  })
+  local rustfmt = require('efmls-configs.formatters.rustfmt')
+
+  -- Lua
   lspconfig.lua_ls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+    capabilities = capabilities, on_attach = on_attach,
     Lua = {
       diagnostics = { globals = { "vim" } },
-      -- workspace = { library = {
-      --   [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-      --   [vim.fn.stdpath("config") .. "/lua"] = true,
-      -- }},
+      workspace = { library = {
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        [vim.fn.stdpath("config") .. "/lua"] = true,
+      }},
     },
   })
 	local luacheck = require("efmls-configs.linters.luacheck")
 	local stylua = require("efmls-configs.formatters.stylua")
+
+  -- C / C++
+  lspconfig.clangd.setup({
+    capabilities = capabilities, on_attach = on_attach,
+    cmd = { "clangd", "--offset-encoding=utf-16" },
+  })
+  local clang_tidy = require('efmls-configs.linters.clang_tidy')
+  local clang_format = require('efmls-configs.formatters.clang_format')
+
+  -- Php
+  lspconfig.phpactor.setup({
+    capabilities = capabilities, on_attach = on_attach,
+  })
+
+  -- Nix
+  lspconfig.nixd.setup({
+    capabilities = capabilities, on_attach = on_attach,
+  })
+  local statix = require('efmls-configs.linters.statix')
 
 	lspconfig.efm.setup({
 		capabilities = capabilities,
 		on_attach = on_attach,
 		settings = {
 			languages = {
+        python = { ruff_l, ruff_f },
+        rust = { rustfmt },
 				lua = { luacheck, stylua },
+        c = { clang_tidy, clang_format }, cpp = { clang_tidy, clang_format },
+        php = { },
+        nix = { statix },
       },
     },
   })
--- .config/nvim/lua/user/format-on-save.lua
+++ .config/nvim/lua/user/format-on-save.lua
@@ -1,13 +1,15 @@
-local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = lsp_fmt_group,
-  callback = function()
-    local efm = vim.lsp.get_active_clients({ name = "efm" })
+-- local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   group = lsp_fmt_group,
+--   callback = function()
+--     local efm = vim.lsp.get_active_clients({ name = "efm" })
+--
+--     if vim.tbl_isempty(efm) then
+--       return
+--     end
+--
+--     vim.lsp.buf.format({ name = "efm", async = true })
+--   end,
+-- })
 
-    if vim.tbl_isempty(efm) then
-      return
-    end
-
-    vim.lsp.buf.format({ name = "efm", async = true })
-  end,
-})
+-- vim.cmd [[autocmd BufWritePost <buffer> lua vim.lsp.buf.format()]]
