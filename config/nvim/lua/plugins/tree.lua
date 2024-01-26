6722d8a5a70a79f6d7088e757fe48b6ad07d13b5
SkohTV <skookie.omg.lol@gmail.com>
2024-01-26 13:26:38 +0100

    treewide: updated nvim plugins

--- config/nvim/lua/plugins/tree.lua
+++ config/nvim/lua/plugins/tree.lua
@@ -1,7 +1,16 @@
+local config = function()
+  -- Check doc for why
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+
+  require("nvim-tree").setup()
+end
+
+
 return {
 	"nvim-tree/nvim-tree.lua",
 	lazy = false,
-  config = true,
+  config = config,
   keys = {
     vim.keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>"),
     vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>"),
