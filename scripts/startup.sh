bdac59ca77fe342d9a0a38873eebec167d077dc9
SkohTV <skookie.omg.lol@gmail.com>
2024-03-24 15:39:57 +0100

    added qflipper

--- scripts/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-obsidian &
-alacritty --class alacritty-home -e zsh -c "sh $HOME/dev/scripts/zellij-init.sh home; exec $SHELL" &
-alacritty --class alacritty-nvim -e zsh -c "sh $HOME/dev/scripts/zellij-init.sh nvim; exec $SHELL" &
-alacritty --class alacritty-main -e zsh -c "sh $HOME/dev/scripts/zellij-init.sh main; exec $SHELL" &
-brave &
-discord --disable-gpu &
