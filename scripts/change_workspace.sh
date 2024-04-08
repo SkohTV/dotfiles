3750cb885311545a9894928fe0af26454c7a9923
SkohTV <skookie.omg.lol@gmail.com>
2024-04-08 22:02:20 +0200

    treewide: new workspace modules ! (screenshare is insane)

--- scripts/change_workspace.sh
+++ scripts/change_workspace.sh
@@ -13,14 +13,21 @@ case "$1" in
   9)
     if [ ! "$workspace" = "9" ]; then
       hyprctl keyword monitor ',highres,1920x1080,1,mirror,eDP-1'
+      pkill hyprpaper
+      /home/qlpth/.config/eww/launch_eww.sh
     fi
-    hyprctl dispatch workspace "$1" &> /dev/null;
+    hyprctl dispatch workspace "$1"
   ;;
   *)
     if [ "$workspace" = "9" ]; then
       hyprctl keyword monitor ',highres,1920x1080,1'
+      sleep 0.1
+      hyprctl dispatch moveworkspacetomonitor 9 'HDMI-A-1'
+      pkill hyprpaper
+      /home/qlpth/.config/eww/launch_eww.sh
     fi
-    hyprctl dispatch workspace "$1" &> /dev/null;
+    hyprctl dispatch workspace 9
+    hyprctl dispatch workspace "$1"
   ;;
 esac
 
