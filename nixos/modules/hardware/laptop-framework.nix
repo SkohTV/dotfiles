90f0fd882bad453748fad43f0268726366b55ca8
SkohTV <noe.lorretdespret+github@proton.me>
2024-10-09 13:35:34 +0200

    new nixos build

--- nixos/modules/hardware/laptop-framework.nix
+++ nixos/modules/hardware/laptop-framework.nix
@@ -1,39 +1,48 @@
 { config, lib, modulesPath, ... }:
 
 {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  # swapDevices = [{
-  #   device = "/swapfile";
-  #   size = 16*1024; # 16G
-  # }];
-  # boot.resumeDevice = "/dev/nvme0n1p2";
-  # boot.kernelParams = [ "resume_offset="];
+  fileSystems."/" =
+    { device = "zpool/root";
+      fsType = "zfs";
+    };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/93aae3a2-2e24-4918-b07a-7dd7286f97da";
-    fsType = "ext4";
-  };
+  fileSystems."/home" =
+    { device = "zpool/home";
+      fsType = "zfs";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/85A6-A39D";
-    fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" ];
-  };
+  fileSystems."/nix" =
+    { device = "zpool/nix";
+      fsType = "zfs";
+    };
 
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/2B14-1A90";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
 
+  swapDevices = [{
+    device = "/dev/disk/by-label/swap";
+  }];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
 }
