8723375bca2278079c589049176b1fde74d73d04
SkohTV <noe.lorretdespret+github@proton.me>
2024-10-09 13:36:00 +0200

    symlink script update

--- symlinks.sh
+++ symlinks.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 
-fn_unlink_link(){
-  
-  src="${HOME}/dev/repo/dotfiles/config/${2}"
-  dest="${HOME}/${1}/${2}"
+
+__fn_unlink_link(){
+
+  src="$1"
+  dest="$2"
 
   if ! test -f "$src" && ! test -d "$src"; then
     echo "$dest does not exist"
@@ -14,8 +15,19 @@ fn_unlink_link(){
   [ -L "$dest" ] && unlink "$dest"
   ln -s "$src" "$dest"
   echo "Linked $2"
-
 }
+
+
+fn_unlink_link(){
+  src="${HOME}/dev/repo/dotfiles/config/${2}"
+  dest="${HOME}/${1}/${2}"
+  __fn_unlink_link "$src" "$dest"
+}
+
+
+
+
+REPO_PATH="$HOME/dev/repo/dotfiles"
 
 fn_unlink_link '.config' 'ags'
 fn_unlink_link '.config' 'alacritty'
@@ -27,9 +39,13 @@ fn_unlink_link '.config' 'lazygit'
 fn_unlink_link '.config' 'nvim'
 fn_unlink_link '.config' 'tofi'
 fn_unlink_link '.config' 'wlogout'
-fn_unlink_link '.config' 'xkb'
 fn_unlink_link '.config' 'zellij'
 fn_unlink_link '' '.zshrc'
-fn_unlink_link '' 'ags'
 fn_unlink_link '.config' 'okularrc'
 fn_unlink_link '.config' 'starship.toml'
+
+__fn_unlink_link "$REPO_PATH/scripts" "$HOME/dev/scripts"
+__fn_unlink_link "$REPO_PATH/wallpapers/desktop" "$HOME/.config/wallpapers"
+__fn_unlink_link "$REPO_PATH/icons" "$HOME/.local/share/icons"
+
+echo 'Finished'
