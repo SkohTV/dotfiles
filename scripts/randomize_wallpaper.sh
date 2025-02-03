2980e88a88c2b097093b62320b23fccfd05983ea
SkohTV <qlpth+github@proton.me>
2025-02-03 11:40:52 +0100

    treewide: added back wallpaper randomizer

---
+++ scripts/randomize_wallpaper.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+a=$(date +"%d")
+b=$(date +"%m")
+c=$(date +"%y")
+
+get_seeded_random()
+{
+  seed="$1"
+  openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt \
+    </dev/zero 2>/dev/null
+}
+
+val=$(($a + $b*10 + $c*100))
+new_wp=$(ls "$HOME/.config/wallpapers/" | grep -i -E '^d+*' | shuf -n 1 --random-source=<(get_seeded_random $val))
+
+unlink /home/skoh/.config/wallpapers/main
+ln -s $new_wp /home/skoh/.config/wallpapers/main 
+
+echo "Refreshed wallpaper"
+
+hyprpaper
