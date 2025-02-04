30e18dfe2ef5ef9806e0679ed9e775991cb9506e
SkohTV <qlpth+github@proton.me>
2025-02-04 11:20:57 +0100

    treewide: arrows begone

--- config/nvim/lua/user/keymap.lua
+++ config/nvim/lua/user/keymap.lua
@@ -5,10 +5,15 @@ vim.keymap.set("n", "<A-k>", "<C-w>k", opts) -- Navigate Up
 vim.keymap.set("n", "<A-l>", "<C-w>l", opts) -- Navigate Right
 
 -- Window Management
-vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
-vim.keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)          -- Split Vertically
+vim.keymap.set("n", "<leader>sh", ":split<CR>", opts)           -- Split Horizontally
 vim.keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimise
 
 -- Indenting
 vim.keymap.set("v", "<", "<gv") -- Shift Indentation to Left
 vim.keymap.set("v", ">", ">gv") -- Shift Indentation to Right
+
+vim.keymap.set({ "n", "v", "i" }, "<Up>", "<Nop>")
+vim.keymap.set({ "n", "v", "i" }, "<Down>", "<Nop>")
+vim.keymap.set({ "n", "v", "i" }, "<Left>", "<Nop>")
+vim.keymap.set({ "n", "v", "i" }, "<Right>", "<Nop>")
