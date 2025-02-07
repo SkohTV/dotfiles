8e6c738bf725cf953a0e40aa7669f71152609e97
SkohTV <qlpth+github@proton.me>
2025-02-07 16:12:46 +0100

    treewide: fixed cpu %

--- config/ags/left/cpu.js
+++ config/ags/left/cpu.js
@@ -1,18 +1,24 @@
 /**
- * @param {any} val
+ * @param {string} val
  */
 function transform(val) {
 
-  return `   na%`
+    val = val.split('\n')
+        .find(l => l.includes('Cpu(s)')) ?? ''
+
+    val = val.split(/\s+/)[1]
+        .replace(',', '.')
+
+    return `   ${val}%`
 }
 
 
 const cpu = Variable('', {
-  poll: [2000, 'mpstat -o JSON'],
+    poll: [2000, 'top -bn1'],
 });
 
 
 export const Cpu = () => Widget.Label({
-  label: cpu.bind().as(transform),
-  class_name: 'cpu_label',
+    label: cpu.bind().as(transform),
+    class_name: 'cpu_label',
 });
