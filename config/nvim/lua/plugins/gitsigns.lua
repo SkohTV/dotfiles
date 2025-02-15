c7ef8c23ab89d4084e874c4bd1403e508d664690
SkohTV <qlpth+github@proton.me>
2025-02-15 23:48:15 +0100

    treewide: removed nvim

--- config/nvim/lua/plugins/gitsigns.lua
+++
@@ -1,9 +0,0 @@
-local config = function()
-  require("gitsigns").setup()
-end
-
-return {{
-  "lewis6991/gitsigns.nvim",
-  event = "VeryLazy",
-  config = config,
-}}
--- config/nvim/lua/user/init.lua
+++
@@ -1,6 +0,0 @@
-require("user.globals")
-require("user.options")
-require("user.keymap")
-require("user.lazy")
-require("user.highlight-on-yank")
-require("user.commands")
