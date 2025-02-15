02a5687731f24d35ca75c3323642047adf059920
SkohTV <qlpth+github@proton.me>
2025-02-15 20:35:16 +0100

    fixed volume ags !

--- config/ags/right/volume.js
+++ config/ags/right/volume.js
@@ -1,80 +1,76 @@
-const label = Variable(Math.round(Number(Utils.exec(`pamixer --get-volume`))))
-const icon = Variable('󰝟')
-const is_mute = Variable(Boolean(Utils.exec(`pamixer --get-mute`)))
+const audio = await Service.import('audio')
 
 
-const pick_icon = () => {
-  if (is_mute.value)
-    return '󰝟'
-  if (label.value > 66)
-    return '󰕾'
-  if (label.value > 33)
-    return '󰖀'
-  return '󰕿'
+const pick_icon = (volume, is_muted) => {
+    if (is_muted)
+        return '󰝟'
+    if (volume > 0.66)
+        return '󰕾'
+    if (volume > 0.33)
+        return '󰖀'
+    return '󰕿'
 }
 
-const change = ({ value }) => {
-  label.value = Math.round(value)
-  icon.value = pick_icon()
-  Utils.exec(`pamixer --set-volume ${label.value}`)
-}
 
-const swap = () => {
-  is_mute.value = !is_mute.value
-
-  if (is_mute.value)
-    Utils.exec(`pamixer --mute`)
-  else
-    Utils.exec(`pamixer --unmute`)
-
-  icon.value = pick_icon()
-}
-
+const change = ({ value }) => audio.speaker.volume = value / 100;
+const swap = () => audio.speaker.is_muted = !audio.speaker.is_muted;
 
 
 export const Volume = () => {
 
-  // Revealer
-  const Reveal = Widget.Revealer({
-    revealChild: false,
-    transitionDuration: 700,
-    transition: 'slide_right',
-    child: Widget.Slider({
-      min: 0,
-      max: 99,
-      value: label.bind(),
-      draw_value: false,
-      on_change: change,
-    })
-  });
+    // Revealer
+    const Reveal = Widget.Revealer({
+        revealChild: false,
+        transitionDuration: 700,
+        transition: 'slide_right',
+        child: Widget.Slider({
+            min: 0,
+            max: 99,
+            draw_value: false,
+            value: audio.speaker.bind('volume').transform(x => x * 100),
+            on_change: change,
+        })
+    });
 
-  // Widget to return
-  const Volume_widget = Widget.EventBox({
-    on_hover: () => Reveal.reveal_child = true,
-    on_hover_lost: () => Reveal.reveal_child = false,
+    // Widget to return
+    const Volume_widget = Widget.EventBox({
+        on_hover: () => Reveal.reveal_child = true,
+        on_hover_lost: () => Reveal.reveal_child = false,
 
-    child: Widget.Box({
-      class_name: is_mute.bind().transform(x => x ? 'mute' : 'sound'),
-      children: [
+        child: Widget.Box({
+            class_name: audio.speaker.bind('is_muted').transform(
+                x => x ? 'mute' : 'sound'
+            ),
 
-        Widget.EventBox({
-
-          child: Widget.Box({
             children: [
-              Widget.Label({ label: icon.bind()}),
-              Widget.Label({label: label.bind().transform(x => x.toString().padStart(2, '0'))}),
+
+                Widget.EventBox({
+
+                    child: Widget.Box({
+                        children: [
+                            Widget.Label({
+                                label: Utils.merge(
+                                    [audio.speaker.bind('volume'), audio.speaker.bind('is_muted')],
+                                    pick_icon
+                                )
+                            }),
+                            Widget.Label({
+                                label: audio.speaker.bind('volume').transform(
+                                    x => (Math.round(x * 100)).toString().padStart(2, '0')
+                                )
+                            }),
+                        ]
+                    }),
+
+                    on_primary_click: swap,
+                }),
+
+                Reveal
             ]
-          }),
+        })
+    });
 
-          on_primary_click: swap,
-        }),
-
-        Reveal
-      ]
-    })
-  });
-
-  // Temporary hover fix
-  Volume_widget.connect('leave-notify-event', _ => Reveal.reveal_child = false);
-  return Volume_widget
+    // Temporary hover fix
+    Volume_widget.connect('leave-notify-event', _ => Reveal.reveal_child = false);
+    return Volume_widget
 }
