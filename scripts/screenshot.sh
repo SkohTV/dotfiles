d9385db40ed35452d98d675eb18544ad029a37c4
SkohTV <qlpth+github@proton.me>
2025-02-15 19:51:07 +0100

    treewide: up screenshot.sh

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-grim -g "$(slurp)" "/tmp/screenshot.png" && wl-copy < /tmp/screenshot.png
+grim -g "$(slurp)" "/tmp/screenshot.png" && wl-copy < /tmp/screenshot.png && imv /tmp/screenshot.png
