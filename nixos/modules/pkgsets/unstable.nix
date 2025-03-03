75c95e488f37308b8a15bb9f50916b3199e6aed2
SkohTV <qlpth+github@proton.me>
2025-03-03 12:59:53 +0100

    treewide: nixwip

--- nixos/modules/pkgsets/unstable.nix
+++ nixos/modules/pkgsets/unstable.nix
@@ -2,5 +2,17 @@ let
   unstable = import <nixos-unstable> {};
 in {
   environment.systemPackages = with unstable; [
+
+    # ags # bundler
+    # gjs # start ?
+    #
+    # gobject-introspection # for imports
+    # wrapGAppsHook
+    #
+    #
+    # astal.astal3 # core 
+    # astal.io # io
+    # astal.hyprland # hyprland module
+
   ];
 }
