b2b29f67aa6756ee535c42d0903751b9e6248c43
SkohTV <qlpth+github@proton.me>
2025-05-03 19:05:10 +0200

    bpfilter overlay

---
+++ nixos/overlays/bpfilter/flake.nix
@@ -0,0 +1,34 @@
+{
+inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+};
+
+
+outputs = {
+  self,
+  nixpkgs,
+  flake-utils,
+}:
+
+
+flake-utils.lib.eachDefaultSystem (system:
+
+    let
+        pkgs = nixpkgs.legacyPackages.${system};
+        bpfilter = pkgs.callPackage ./default.nix { };
+
+    in {
+        packages = {
+            inherit bpfilter;
+            default = bpfilter;
+        };
+
+        devShells = {
+            default = pkgs.mkShell { inputsFrom = [ bpfilter ]; };
+        };
+    }
+);
+
+
+}
