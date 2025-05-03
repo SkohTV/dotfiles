897342b389ee6324965db64a31389126b1109ae4
SkohTV <qlpth+github@proton.me>
2025-05-03 19:05:10 +0200

    treewide: bpfilter overlay

---
+++ nixos/overlays/bpfilter/default.nix
@@ -0,0 +1,46 @@
+{
+stdenv,
+lib,
+
+cmake,
+git,
+pkg-config,
+libbpf,
+libnl,
+bison,
+flex,
+doxygen,
+lcov,
+sphinx,
+cmocka,
+clang-tools,
+gbenchmark,
+libgit2
+}:
+
+
+stdenv.mkDerivation {
+  pname = "bpfilter";
+  version = "1.0.0";
+
+  src = ./bpfilter;
+
+  nativeBuildInputs = [
+    cmake
+    git
+    pkg-config
+    libbpf
+    libnl
+    bison
+    flex
+    doxygen
+    lcov
+    sphinx
+    cmocka
+    clang-tools
+    gbenchmark
+    libgit2
+  ];
+
+  cmakeFlags = [ (lib.cmakeBool "DCMAKE_EXPORT_COMPILE_COMMANDS" 1) ];
+}
