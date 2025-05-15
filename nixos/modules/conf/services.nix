8daee42e31af12a55fa3c44b3d014645016711d9
SkohTV <qlpth+github@proton.me>
2025-05-15 08:57:47 +0200

    fixed timer

--- nixos/modules/conf/services.nix
+++ nixos/modules/conf/services.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
 
   ### SERVICES
@@ -27,19 +27,29 @@
 
 
     services."daily-chores" = {
+      path = ["/run/current-system/sw"];
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "skoh";
+      };
+
       script = ''
         # Commit obsidian vault
-        cd /home/skoh/dev/repo/vault/
-        git add . && git commit -m "Auto commit: $(date +'%a, %d %b %Y')" && git push origin
+        eval "$(ssh-agent -s)"
+        ssh-add ~/.ssh/github
+
+        ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ add .
+        ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ diff-index --quiet HEAD ||\
+            ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ commit -m "Auto commit: $(date +'%a, %d %b %Y')"
+        ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ push origin
+
 
         # Change wp
+        export HYPRLAND_INSTANCE_SIGNATURE=$(hyprctl instances -j | jq -r '.[0].instance')
         /home/skoh/dev/scripts/randomize_wallpaper.sh
         hyprctl hyprpaper reload eDP-1,/home/skoh/.config/wallpapers/main 
       '';
-      serviceConfig = {
-        Type = "oneshot";
-        User = "root";
-      };
     };
 
   };
