2730e3af1d4d3712d6a45795e5c085dd5c2ac029
SkohTV <qlpth+github@proton.me>
2025-05-18 22:14:06 +0200

    treewide: updated daily-chore script

--- nixos/modules/conf/services.nix
+++ nixos/modules/conf/services.nix
@@ -6,11 +6,6 @@
     flatpak.enable = true; # Flatpaks
     upower.enable = true; # Power stuff
     openssh.enable = true; # ssh
-
-    ollama = { # ollama for nvim
-      enable = true;
-      acceleration = "rocm";
-    };
   };
 
 
@@ -35,6 +30,7 @@
       };
 
       script = ''
+
         # Commit obsidian vault
         eval "$(ssh-agent -s)"
         ssh-add ~/.ssh/github
@@ -42,13 +38,18 @@
         ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ add .
         ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ diff-index --quiet HEAD ||\
             ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ commit -m "Auto commit: $(date +'%a, %d %b %Y')"
-        ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ push origin
 
 
         # Change wp
         export HYPRLAND_INSTANCE_SIGNATURE=$(hyprctl instances -j | jq -r '.[0].instance')
         /home/skoh/dev/scripts/randomize_wallpaper.sh
         hyprctl hyprpaper reload eDP-1,/home/skoh/.config/wallpapers/main 
+
+
+        # Wait for network connection before vault push
+        sleep 60
+        ${pkgs.git}/bin/git -C /home/skoh/dev/repo/vault/ push origin
+
       '';
     };
 
