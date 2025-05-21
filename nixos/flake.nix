b0cce7e96b472fcb0cba57152a657f3f6b8a5e14
SkohTV <qlpth+github@proton.me>
2025-05-21 13:48:26 +0200

    treewide: fully flaked config

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,13 +1,31 @@
 { inputs = {
+
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-  };
 
-
-  outputs = {nixpkgs, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./config.nix ];
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
+
+
+  outputs = {nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./config.nix ];
+      };
+
+      homeConfigurations.skoh = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [ ./home.nix ];
+
+      };
+
+    };
 
 }
