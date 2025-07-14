e97aabbcbcf299718096ba7d43cc5b36dfe1f1f0
SkohTV <qlpth+github@proton.me>
2025-07-14 17:13:06 +0200

    treewide: luasnip new snippet

--- config/nvim/lua/plugins/lsp/snippets.lua
+++ config/nvim/lua/plugins/lsp/snippets.lua
@@ -1,5 +1,29 @@
 local config = function ()
-   require("luasnip.loaders.from_vscode").lazy_load()
+  local ls = require("luasnip")
+  local s = ls.snippet
+  local t = ls.text_node
+  local i = ls.insert_node
+
+  require("luasnip.loaders.from_vscode").lazy_load()
+
+
+  local map_leaderpp = function(filetype, snippet)
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = filetype,
+      callback = function ()
+        vim.keymap.set("n", "<leader>pp", function()
+          ls.snip_expand(snippet)
+        end)
+      end,
+    })
+  end
+
+
+  map_leaderpp('python', s('', { t({'print("'}), i(1), t({'")'}) }))
+  map_leaderpp('rust', s('', { t({'println!("'}), i(1), t({'");'}) }))
+  map_leaderpp('javascript', s('', { t({'console.log("'}), i(1), t({'");'}) }))
+  map_leaderpp('c', s('', { t({'printf("'}), i(1), t({'");'}) }))
+
 end
 
 
