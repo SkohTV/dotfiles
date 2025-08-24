86452e7aa509a47248a8a4e2c948ed4bc8799847
SkohTV <qlpth+github@proton.me>
2025-08-24 08:17:20 -0400

    config(ags): remove old config

--- config/ags.bak/left/cava.js
+++
@@ -1,9 +0,0 @@
-const cava = Variable('', {
-  listen: `${App.configDir}/scripts/cava.sh`,
-});
-
-
-export const Cava = () => Widget.Label({
-  label: cava.bind(),
-  class_name: 'cava_label',
-});
--- config/ags.bak/shared/separator.js
+++
@@ -1,6 +0,0 @@
-export function Split_generator() {
-  return Widget.Label({
-    label: "|",
-    class_name: 'split'
-  });
-}
