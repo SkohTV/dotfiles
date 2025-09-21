fc592d81315ca57089764cb6e55cb04b36f2ff2a
SkohTV <contact@skoh.dev>
2025-09-21 13:30:10 -0400

    nixos(overlays): better bpfilter build pipeline

--- nixos/overlays/bpfilter/default.nix
+++ nixos/overlays/bpfilter/default.nix
@@ -1,52 +1,84 @@
 {
-stdenv,
-lib,
-
-cmake,
-git,
-pkg-config,
-libbpf,
-libnl,
-bison,
-flex,
-doxygen,
-lcov,
-sphinx,
-cmocka,
-clang-tools,
-gbenchmark,
-libgit2,
-libelf,
-zlib,
-clang,
-xxd
+  bison,
+  clangStdenv,
+  cmake,
+  cmocka,
+  doxygen,
+  flex,
+  gitMinimal,
+  lcov,
+  lib,
+  libbpf,
+  libelf,
+  libnl,
+  nix-update-script,
+  pkg-config,
+  sphinx,
+  tinyxxd,
+  zlib,
 }:
 
-
-stdenv.mkDerivation {
+clangStdenv.mkDerivation (finalAttrs: {
   pname = "bpfilter";
-  version = "1.0.0";
+  version = "0.5.2";
 
   src = ./bpfilter;
 
   nativeBuildInputs = [
-    cmake
-    git
-    pkg-config
-    libbpf
-    libnl
     bison
-    flex
-    doxygen
-    lcov
-    sphinx
+    cmake
     cmocka
-    clang-tools
-    gbenchmark
-    libgit2
-    libelf
-    zlib
-    clang
-    xxd
+    doxygen
+    flex
+    lcov
+    pkg-config
+    sphinx
+    tinyxxd
+
+    # bpfilter's cmake files requires this, even if we specify the version
+    # we're using without needing to detect the version during the build.
+    gitMinimal
   ];
-}
+
+  buildInputs = [
+    libbpf
+    libelf
+    libnl
+    zlib
+  ];
+
+  cmakeFlags = [
+    "-DDEFAULT_PROJECT_VERSION=${finalAttrs.version}"
+    "-DNO_TESTS=1" # tries to clone nftables
+    "-DNO_CHECKS=1"
+    "-DNO_BENCHMARKS=1"
+  ];
+
+  outputs = [
+    "out"
+    "lib"
+    "dev"
+  ];
+
+  # invalid with -target bpf
+  hardeningDisable = [ "zerocallusedregs" ];
+
+  preFixup = ''
+    substituteInPlace $out/lib/systemd/system/bpfilter.service --replace-fail /usr/sbin/bpfilter $out/bin/bpfilter
+
+    # workaround for https://github.com/NixOS/nixpkgs/issues/144170
+    substituteInPlace $lib/lib/pkgconfig/bpfilter.pc --replace-fail \''${prefix}/ ""
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "BPF-based packet filtering framework";
+    homepage = "https://bpfilter.io";
+    changelog = "https://github.com/facebook/bpfilter/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.gpl2Only;
+    maintainers = [ lib.maintainers.jmbaur ];
+    mainProgram = "bpfilter";
+    platforms = lib.platforms.linux;
+  };
+})
